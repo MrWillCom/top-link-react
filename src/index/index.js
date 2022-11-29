281fa415cfedcf04a5ea11206ae2d7519d3df95f
lookcos <lookcos@gmail.com>
2022-11-29 11:42:48 +0800

    update

--- src/index/index.js
+++ src/index/index.js
@@ -1,10 +1,7 @@
 import "./index.css"
-import { Button } from "../components/button"
 import React from "react"
 
 export default function Index() {
-
-    
 
 
     return (
@@ -20,7 +17,7 @@ export default function Index() {
                                             <div className="content-text">
                                                 <div className="text-wraper">
                                                     <div className="text-title">
-                                                        <h2 className="text-h1 text-colorful">THE TOP LINK  您的专属数字名片</h2>
+                                                        <div className="text-h1 text-colorful">THE TOP LINK  您的专属数字名片</div>
                                                     </div>
                                                     <div className="text-subtitle">
                                                        <div className="subtitle-wraper">
@@ -39,11 +36,11 @@ export default function Index() {
                                         </div>
                                         <div className="content-extra">
                                                 <div className="extra-wraper">
-                                                   <div className="extra-box" >
-                                                   <img src="/images/index/profile_blue.png"></img>
-                                                    <img src="/images/index/profile_dark.png"></img>
-                                                    <img src="/images/index/profile_colorful.png"></img>
-                                                    <img src="/images/index/profile_orange.png"></img>
+                                                   <div className="extra-box extra-animotion" >
+                                                    <img src="/images/index/profile_blue.png" alt="profile_blue"></img>
+                                                    <img src="/images/index/profile_dark.png" alt="dark"></img>
+                                                    <img src="/images/index/profile_colorful.png" alt="colorful"></img>
+                                                    <img src="/images/index/profile_orange.png" alt="organge"></img>
                                                    </div>
                                                 </div>
                                             </div>
@@ -64,7 +61,7 @@ export default function Index() {
                                             <div className="content-text">
                                                 <div className="text-wraper">
                                                     <div className="text-title">
-                                                        <h2 className="text-h1 intro-2-text">Link Everything 链一切</h2>
+                                                        <div className="text-h1">Link Everything 链一切</div>
                                                     </div>
                                                     <div className="text-subtitle">
                                                        <div className="subtitle-wraper">
@@ -73,8 +70,8 @@ export default function Index() {
                                                          </div>
                                                        </div>
                                                     </div>
-                                                    <div className="text-button">
-
+                                                    <div className="text-button-common">
+                                                        <button>免费注册</button>
                                                     </div>
                                                 </div>
                                                 
@@ -83,11 +80,8 @@ export default function Index() {
                                         </div>
                                         <div className="content-extra">
                                                 <div className="extra-wraper extra-wraper-reverse">
-                                                   <div className="extra-box" >
-                                                   <img src="/images/index/profile_blue.png"></img>
-                                                    <img src="/images/index/profile_dark.png"></img>
-                                                    <img src="/images/index/profile_colorful.png"></img>
-                                                    <img src="/images/index/profile_orange.png"></img>
+                                                   <div className="extra-common" >
+                                                   <img src="/images/index/intro3.png" alt="blue"></img>
                                                    </div>
                                                 </div>
                                             </div>
@@ -99,7 +93,51 @@ export default function Index() {
                 </div>
 
 
-
+                <div className="intro intro-3">
+                    <div className="intro-box">
+                        <div className="intro-wraper">
+                            <div className="intro-content">
+                                <div className="intro-color">
+                                    <div className="content-main">
+                                        <div className="content-wraper">
+                                            <div className="content-text">
+                                                <div className="text-wraper">
+                                                    <div className="text-title">
+                                                        <div className="text-h1">Explore space 探索你关注的领域IP</div>
+                                                    </div>
+                                                    <div className="text-subtitle">
+                                                       <div className="subtitle-wraper">
+                                                         <div className="subtitle-box">
+                                                            <p>搜索你喜欢的自媒体、艺人、社群、品牌...</p>
+                                                         </div>
+                                                       </div>
+                                                    </div>
+                                                    <div className="text-button-common">
+                                                        <button>开始探索</button>
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                          
+                                        </div>
+                                        <div className="content-extra">
+                                                <div className="extra-wraper">
+                                                   <div className="extra-common" >
+                                                    <img src="/images/index/intro4.png" alt="blue"></img>
+                                                   </div>
+                                                </div>
+                                            </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <div className="footer-wraper">
+                        <p>&copy;THE.TOP 2019 - 2022</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
